8429b9774948c6fa1866ebddacb4eecdbd245247
Michael Rans <rans@email.com>
2016-11-29 14:46:57 +0100

    aiohttp version

--- run.py
+++ run.py
@@ -11,69 +11,66 @@ import logging
 import time
 import datetime
 
-import grequests
+import asyncio
+
+import async_timeout
+import uvloop
+import aiohttp
+import hashlib
 from dateutil import parser
-from requests import HTTPError
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 from hdx.data.resource import Resource
 from hdx.facades.simple import facade
-from hdx.utilities.downloader import Download, DownloadError
 
 from database.base import Base
 from database.dbresource import DBResource
 
 logger = logging.getLogger(__name__)
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 engine = create_engine('sqlite:///resources.db', echo=False)
 Session = sessionmaker(bind=engine)
+md5hash = hashlib.md5()
+
+async def fetch(metadata, session):
+    url, resource_id = metadata
+    try:
+        with async_timeout.timeout(10, loop=session.loop):
+            async with session.get(url) as resp:
+                logger.info(url)
+                if resp.status != 200:
+                    return resource_id, url, 0, resp.status
+                last_modified = resp.headers.get('Last-Modified', None)
+                if last_modified:
+                    return resource_id, url, 1, last_modified
+                while True:
+                    chunk = await resp.content.read(1024)
+                    if not chunk:
+                        break
+                    md5hash.update(chunk)
+                return resource_id, url, 2, md5hash.hexdigest()
+    except Exception as e:
+        return resource_id, url, 0, str(e)
+
+async def bound_fetch(sem, url, session):
+    # Getter function with semaphore.
+    async with sem:
+        return await fetch(url, session)
 
 
-def set_metadata(metadata):
-    def hook(resp, **kwargs):
-        resp.metadata = metadata
-        return resp
-    return hook
+async def check_resources_for_last_modified(last_modified_check, loop):
+    tasks = list()
 
+    # create instance of Semaphore
+    sem = asyncio.Semaphore(100)
 
-def check_resources_for_last_modified(last_modified_check):
-    results = list()
-    reqs = list()
-
-    def exception_handler(req, exc):
-        url, resource_id = req.metadata
-        results.append((resource_id, url, 0, str(exc)))
-
-    with Download() as download:
+    conn = aiohttp.TCPConnector(conn_timeout=10, limit=10, keepalive_timeout=10)
+    async with aiohttp.ClientSession(connector=conn, loop=loop) as session:
         for metadata in last_modified_check:
-            req = grequests.get(metadata[0], timeout=10, session=download.session, callback=set_metadata(metadata))
-            req.metadata = metadata
-            reqs.append(req)
-        count = 0
-        for resp in grequests.imap(reqs, size=100, stream=True, exception_handler=exception_handler):
-            logger.info('Count = %d' % count)
-            count += 1
-            url, resource_id = resp.metadata
-            try:
-                resp.raise_for_status()
-            except HTTPError as e:
-                results.append((resource_id, url, 0, resp.status_code))
-                resp.close()
-                continue
-            last_modified = resp.headers.get('Last-Modified', None)
-            if last_modified:
-                results.append((resource_id, url, 1, last_modified))
-                resp.close()
-                continue
-            download.response = resp
-            try:
-                md5hash = download.hash_stream(url)
-                results.append((resource_id, url, 2, md5hash))
-            except DownloadError as e:
-                results.append((resource_id, url, 0, str(e)))
-            finally:
-                resp.close()
-    return results
+            task = asyncio.ensure_future(bound_fetch(sem, metadata, session))
+            tasks.append(task)
+        return await asyncio.gather(*tasks)
 
 
 def set_last_modified(dbresource, modified_date):
@@ -83,6 +80,7 @@ def set_last_modified(dbresource, modified_date):
             dbresource.last_modified = dbresource.http_last_modified
     else:
         dbresource.last_modified = dbresource.http_last_modified
+
 
 def main(configuration):
     ''''''
@@ -132,7 +130,9 @@ def main(configuration):
     session.commit()
     last_modified_check = sorted(last_modified_check)
     start_time = time.time()
-    results = check_resources_for_last_modified(last_modified_check)
+    loop = asyncio.get_event_loop()
+    future = asyncio.ensure_future(check_resources_for_last_modified(last_modified_check, loop))
+    results = loop.run_until_complete(future)
     logger.info('Execution time: %s seconds' % (time.time() - start_time))
     lastmodified_count = 0
     hash_updated_count = 0
