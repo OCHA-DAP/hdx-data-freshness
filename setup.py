f77a5432c2bc808c36c6c8625390159e3fe004ff
Michael Rans <rans@email.com>
2017-02-15 11:51:47 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+requirements = ['aiodns',
+                'aiohttp',
+                'cchardet',
+                'tqdm',
+                'hdx-python-api'
+                ]
+
+setup(
+    name='hdx-data-freshness',
+    version='0.1',
+    packages=find_packages(exclude=['ez_setup', 'tests', 'tests.*']),
+    url='http://data.humdata.org/',
+    license='PSF',
+    author='Michael Rans',
+    author_email='rans@email.com',
+    description='HDX Data Freshness',
+    dependency_links=[
+        'https://github.com/ocha-dap/hdx-python-api/zipball/master#egg=hdx-python-api'
+    ],
+    install_requires=requirements,
+    package_data={
+        # Include any package contains *.yml files, include them:
+        '': ['*.yml'],
+    },
+    include_package_data=True,
+)
