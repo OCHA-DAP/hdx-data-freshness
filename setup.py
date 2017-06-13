54aad4274c2e6dfcbfdf16c66697a3914510d162
Michael Rans <rans@email.com>
2017-06-13 14:11:51 +0200

    Update hdx python dependency version

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     description='HDX Data Freshness',
     license='MIT',
     url='https://github.com/OCHA-DAP/hdx-data-freshness',
-    version='0.86',
+    version='0.87',
     author='Michael Rans',
     author_email='rans@email.com',
     keywords=['HDX', 'fresh', 'freshness', 'data freshness'],
