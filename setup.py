bd06b69baa57be0480ff4ec410080ea9492ad8fc
Mike <rans@email.com>
2018-03-05 16:47:56 +0100

    User agent Remove connection timeout

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     description='HDX Data Freshness',
     license='MIT',
     url='https://github.com/OCHA-DAP/hdx-data-freshness',
-    version='1.0.4',
+    version='1.0.5',
     author='Michael Rans',
     author_email='rans@email.com',
     keywords=['HDX', 'fresh', 'freshness', 'data freshness'],
