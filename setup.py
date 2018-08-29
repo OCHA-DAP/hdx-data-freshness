7e965b6c9873b23e7ca776c8370f392ff108cb3a
Mike <rans@email.com>
2018-08-29 14:41:00 +0200

    Update requirements

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ requirements = ['aiodns',
                 'psycopg2-binary',
                 'tqdm',
                 'uvloop',
-                'hdx-python-api>=3.1.9'
+                'hdx-python-api>=3.2.4'
                 ]
 
 classifiers = [
@@ -70,7 +70,7 @@ setup(
     description='HDX Data Freshness',
     license='MIT',
     url='https://github.com/OCHA-DAP/hdx-data-freshness',
-    version='1.1.9',
+    version='1.2.0',
     author='Michael Rans',
     author_email='rans@email.com',
     keywords=['HDX', 'fresh', 'freshness', 'data freshness'],
